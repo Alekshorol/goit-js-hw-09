53f64f6061faef7f00dc32cb19585582a58420b1
AleksHor <horosh861@gmail.com>
2023-03-16 09:38:23 +0200

    task 9.1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bodyEl = document.querySelector('body');
 
+bodyEl.addEventListener('click', battonClick);
+
+let intervalId;
+
+function battonClick(evt) {
+  if (evt.target.hasAttribute('data-start')) {
+    intervalId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    bodyEl.querySelector('[data-start]').disabled = true;
+    bodyEl.querySelector('[data-stop]').disabled = false;
+  }
+  if (evt.target.hasAttribute('data-stop')) {
+    clearInterval(intervalId);
+    bodyEl.querySelector('[data-start]').disabled = false;
+    bodyEl.querySelector('[data-stop]').disabled = true;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
